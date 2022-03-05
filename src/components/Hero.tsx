d383769e08b4d2a5c8aada9948525c199f125f52
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-03-05 15:51:16 -0800

    Convert Hero to typescript
     Define prop types for Hero section
    
     Issue#19

---
+++ src/components/Hero.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Section from "./Section";
+import SectionHeader from "./SectionHeader";
+
+interface HeroProps {
+  bgColor?: string;
+  bgImage?: string;
+  bgImageOpacity?: string;
+  size?: string;
+  className?: string;
+  style?: Object;
+  title?: string;
+  subtitle?: string;
+  sectionHeaderProps?: Object;
+  cta?: any;
+}
+function Hero(props: HeroProps) {
+  return (
+    <Section
+      bgColor={props.bgColor}
+      size={props.size}
+      bgImage={props.bgImage}
+      bgImageOpacity={props.bgImageOpacity}
+    >
+      {(props.title || props.subtitle) && (
+        <Container>
+          <Box textAlign="center">
+            <SectionHeader
+              title={props.title}
+              subtitle={props.subtitle}
+              size={4}
+              {...props.sectionHeaderProps}
+            />
+          </Box>
+        </Container>
+      )}
+      {props.cta && (
+        <Box py={5} display="flex" justifyContent="center" alignItems="center">
+          {props.cta}
+        </Box>
+      )}
+    </Section>
+  );
+}
+
+export default Hero;
