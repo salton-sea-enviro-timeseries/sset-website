70ad8a233256ad5f79361a4080feeb95f40eedaf
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-03-05 16:48:45 -0800

    Convert custome document file to typescript
     Change file name and import DocumentContext to define ctx prop.
    
     Issues: #53

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     // Render app and page and get the context of the page with collected side effects.
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
       });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -20,8 +26,8 @@ export default class MyDocument extends Document {
       // Styles fragment is rendered after the app and page rendering finish.
       styles: [
         ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
+        sheets.getStyleElement()
+      ]
     };
   }
 
