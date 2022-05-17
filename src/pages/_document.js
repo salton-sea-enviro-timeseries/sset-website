a85faed8dc34f0c0eb6e3d29e485ff7ad302362d
Diego Bernal <diego91bernal@gmail.com>
2022-05-17 13:06:10 -0700

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
 
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
       });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -20,8 +20,8 @@ export default class MyDocument extends Document {
       // Styles fragment is rendered after the app and page rendering finish.
       styles: [
         ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
+        sheets.getStyleElement()
+      ]
     };
   }
 
@@ -29,6 +29,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link href="/favicon.ico" rel="shortcut icon" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
