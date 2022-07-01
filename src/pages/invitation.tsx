67187ddd9f57a71004d9e8c8d10b049d544bf5e9
Diego Bernal <diego91bernal@gmail.com>
2022-06-30 21:41:18 -0700

    Fix some of the cms config + add redirect

---
+++ src/pages/invitation.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+      </>
+    );
+  }
+}
