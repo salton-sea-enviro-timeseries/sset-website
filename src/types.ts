feb08dd0c1b6b4430e2472d68ea8bbbd3522fb36
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-11-02 16:29:17 -0700

    add contentful types

--- src/types.ts
+++ src/types.ts
@@ -164,3 +164,95 @@ export interface RawNutrientsData {
 }
 
 export type Language = "en" | "es";
+// =================================================== CMS Contentful Types Start======================================
+export type LocaleOption<T> = {
+  "en-US": T;
+  es: T;
+};
+export type LocaleDefault<T> = {
+  "en-US": T;
+};
+
+type Metadata = {
+  metadata: LocaleDefault<{
+    fields: {
+      site_title: LocaleDefault<string>;
+      open_graph_image?: LocaleDefault<string>;
+      open_graph_site_type?: LocaleDefault<string>;
+      open_graph_site_url?: LocaleDefault<string>;
+      open_graph_site_description?: LocaleDefault<string>;
+    };
+  }>;
+};
+
+type Fields<T> = {
+  fields: T;
+};
+
+type BodyValues = {
+  content: [{ value: string }];
+};
+export type NestedObjBodyText = { content: [BodyValues] };
+
+type PageContent = [
+  Fields<{
+    body: LocaleOption<NestedObjBodyText>;
+    title: LocaleOption<string>;
+  }>
+];
+
+type HeroImage = LocaleDefault<
+  Fields<{
+    title: LocaleDefault<string>;
+    file: LocaleDefault<{ url: string }>;
+  }>
+>;
+
+type HeroContent = LocaleDefault<
+  Fields<{
+    subTitle: LocaleOption<string>;
+    title: LocaleOption<string>;
+    buttonText: LocaleOption<string>;
+    heroImage: HeroImage;
+  }>
+>;
+type MenuItem = Fields<{
+  description: LocaleOption<{ content: [BodyValues] }>;
+  name: LocaleOption<string>;
+  unit: LocaleDefault<string>;
+  paramKey: LocaleDefault<string>;
+}>;
+type memberInfo = Fields<{
+  name: LocaleDefault<string>;
+  email: LocaleDefault<string>;
+  website: LocaleDefault<string>;
+  affiliation: HeroImage;
+}>;
+type MenuList = LocaleDefault<[MenuItem]>;
+// ========= Page types ==========================
+export type HomePage = {
+  content: LocaleOption<PageContent>;
+  hero: HeroContent;
+  label: LocaleDefault<string>;
+  metadata: Metadata;
+};
+
+export type DashboardPage = {
+  download_nutrients_data_button: LocaleOption<string>;
+  download_sensor_data_button: LocaleOption<string>;
+  label: LocaleDefault<string>;
+  map_caption_main: LocaleOption<string>;
+  map_caption_secondary: LocaleOption<string>;
+  menuList: MenuList;
+};
+
+export type ContactPage = {
+  members: LocaleDefault<memberInfo[]>;
+  applyLink: LocaleDefault<string>;
+  body: LocaleOption<string>;
+  callToAction: LocaleOption<string>;
+  metadata: Metadata;
+  title: LocaleOption<string>;
+  heroImage: HeroImage;
+};
+// =================================================== CMS Contentful Types End======================================
