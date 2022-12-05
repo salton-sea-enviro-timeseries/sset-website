71b68407512bae67ddd69d2a6e5f1676d8e74726
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-12-05 12:30:07 -0800

    add helper function for quant devices

---
+++ src/lib/quant.ts
@@ -0,0 +1,33 @@
+const MODEL_NUM = "MOD-PM-00404";
+const url = new URL(
+  `https://app.quant-aq.com/ui-device-data/${MODEL_NUM}?table=clean`
+);
+
+export interface MODRawDeviceDataResponse {
+  "geo.lat": number;
+  "geo.lon": number;
+  "met.rh": number;
+  "met.temp": number;
+  "model.pm.pm1": number;
+  "model.pm.pm10": number;
+  "model.pm.pm25": number;
+  pm1: number;
+  pm10: number;
+  pm25: number;
+  sn: string;
+  timestamp: string;
+  timestamp_local: string;
+  url: string;
+}
+
+export async function getQuantDevice() {
+  const options = {
+    method: "GET"
+  };
+
+  const {
+    data: { data },
+    meta
+  } = await (await fetch(url, options)).json();
+  return data[0] as MODRawDeviceDataResponse;
+}
