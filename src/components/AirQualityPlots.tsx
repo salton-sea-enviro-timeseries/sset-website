f48ef2a05b6487f837eeab00dc964cb48e55425a
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-02-25 18:22:07 -0800

    Add air quality plot  WIP: add greater date range       fix styling

--- src/components/AirQualityPlots.tsx
+++ src/components/AirQualityPlots.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useMemo, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,20 +8,31 @@ import {
   Title,
   Tooltip,
   Legend,
-  SubTitle
+  SubTitle,
+  TimeScale,
+  TimeSeriesScale,
+  LineController
 } from "chart.js";
-import { Line, Chart } from "react-chartjs-2";
+import "chartjs-adapter-date-fns";
+import { Line } from "react-chartjs-2";
 import { Device } from "lib/aqmd";
 import { fetcher } from "utils";
-import { promises } from "stream";
 import determineSourceOfData from "lib/determineSourceOfData";
 import useSWR from "swr";
+import { format } from "date-fns";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
+  TimeScale,
+  TimeSeriesScale,
+  LineController,
   Title,
   Tooltip,
   Legend,
@@ -38,8 +49,34 @@ type airQualityDevices = {
   location: string;
   color: string;
 };
+type ParamAQIStandardMap = {
+  O3: number;
+  PM2_5: number;
+  PM10: number;
+  NO2: number;
+};
+const paramAQIStandardMap: ParamAQIStandardMap = {
+  O3: 70,
+  PM2_5: 35,
+  PM10: 150,
+  NO2: 100
+};
+
+const colors = [
+  "#2d647d",
+  "#000000",
+  "#FF69B4",
+  "#00FFFF",
+  "#FFFF00",
+  "#1a1245",
+  "#9400D3",
+  "#FFD700",
+  "#99225f",
+  "#800080"
+];
 const canvasBackgroundColor = {
   id: "canvasBackgroundColor",
+
   beforeDraw(
     chart: {
       ctx: any;
@@ -55,16 +92,19 @@ const canvasBackgroundColor = {
     args: any,
     pluginOptions: any
   ) {
-    // console.log(chart);
     const {
       ctx,
       chartArea: { top, bottom, left, right, width },
       scales: { x, y }
     } = chart;
-
-    // ctx.fillRect(left,top,width,height)
-    function bgColors(bracketLow: number, bracketHigh: number, color: string) {
-      ctx.fillStyle = color;
+    function bgColors(bracketLow: number, bracketHigh: number) {
+      let gradient = ctx.createLinearGradient(0, top, 0, bottom);
+      // Percentage of color from ending point (bottom)
+      // TODO : will need to adjust range based on AQI standard;
+      gradient.addColorStop(0, "rgba(203,70,18,100)"); // red
+      gradient.addColorStop(0.5, "rgba(233,228,22,.5)"); // yellow
+      gradient.addColorStop(1, "rgba(38,195,11,.3)"); //green
+      ctx.fillStyle = gradient;
       ctx.fillRect(
         left,
         y.getPixelForValue(bracketHigh),
@@ -73,263 +113,223 @@ const canvasBackgroundColor = {
       );
       ctx.restore();
     }
-
-    bgColors(60, 90, "rgba(255,26,104,0.2)");
-    bgColors(30, 60, "rgba(255,206,86,0.2)");
-    bgColors(0, 30, "rgba(75,192,192,0.2)");
+    bgColors(0, 300);
   }
 };
-
+// TODO fix typing issues
 export const plugins: any = [canvasBackgroundColor];
-
-export const options = {
-  responsive: true,
-  interaction: {
-    mode: "index" as const,
-    intersect: false
-  },
-  stacked: false,
-  plugins: {
-    title: {
-      display: true,
-      text: "Param Goes Here" as const,
-      font: {
-        size: 16
-      }
+function paramAQICalc(data: any) {
+  // @ts-ignore
+  return data.map(({ EndDate, timestamp, O3, NO2, PM10, "PM2.5": PM2_5 }) => {
+    return {
+      O3: Math.round((O3 / 70) * 100),
+      // leaving NO2 as is
+      NO2: Math.round(NO2),
+      PM10: Math.round((PM10 / 150) * 100),
+      PM2_5: Math.round((PM2_5 / 35) * 100),
+      x: timestamp ? timestamp : EndDate
+    };
+  });
+}
+export const options = (selectedParam: string) => {
+  return {
+    responsive: true,
+    interaction: {
+      mode: "index" as const,
+      intersect: false
     },
-    legend: {
-      position: "bottom" as const
-    },
-    subtitle: {
-      display: true,
-      text: "One Hour Average",
-      padding: {
-        bottom: 8
-      }
-    }
-  },
-  scales: {
-    y: {
+    stacked: false,
+    plugins: {
       title: {
         display: true,
-        text: "AQI Value",
+        text: selectedParam,
         font: {
           size: 16
         }
       },
-      type: "linear" as const,
-      display: true,
-      position: "left" as const,
-      beginAtZero: true,
-      grid: {
-        drawOnChartArea: false
+      legend: {
+        position: "bottom" as const
+      },
+      subtitle: {
+        display: true,
+        text: "One Hour Average",
+        padding: {
+          bottom: 8
+        }
       }
     },
-    x: {
-      beginAtZero: false,
-      grid: {
-        drawOnChartArea: false
+    // TODO: this will be dynamic based on selector
+    parsing: {
+      xAxisKey: "x",
+      yAxisKey: selectedParam
+    },
+    scales: {
+      y: {
+        title: {
+          display: true,
+          // TODO change to e-2 based on selection
+          text: "AQI Value",
+          font: {
+            size: 16
+          }
+        },
+        type: "linear" as const,
+        display: true,
+        position: "left" as const,
+        beginAtZero: true,
+        grid: {
+          drawOnChartArea: false
+        },
+        suggestedMin: 0,
+        suggestedMax: 300
+      },
+      x: {
+        parsing: false,
+        type: "time",
+        time: {
+          unit: "day"
+        },
+        ticks: {
+          minRotation: 0,
+          maxRotation: 0,
+          // @ts-ignore
+          callback: function (val, index) {
+            // @ts-ignore
+            return index % 24 === 0 ? this.getLabelForValue(val) : "";
+          }
+        },
+        beginAtZero: false,
+        grid: {
+          drawOnChartArea: false
+        }
       }
     }
-  }
-};
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-const sensorData = [
-  {
-    id: "sensorid1",
-    param1: 23,
-    param2: 56,
-    param3: 66,
-    timestamp: "date",
-    date: "June 12 2020"
-  },
-  {
-    id: "sensorid1",
-    param1: 26,
-    param2: 28,
-    param3: 11,
-    timestamp: "date",
-    date: "June 12 2020"
-  },
-  {
-    id: "sensorid2",
-    param1: 12,
-    param2: 78,
-    param3: 78,
-    timestamp: "date",
-    date: "July 3 2020"
-  },
-  {
-    id: "sensorid2",
-    param1: 10,
-    param2: 7,
-    param3: 79,
-    timestamp: "date",
-    date: "July 3 2020"
-  },
-  {
-    id: "sensorid3",
-    param1: 45,
-    param2: 23,
-    param3: 98,
-    timestamp: "date",
-    date: "August 10 2020"
-  },
-  {
-    id: "sensorid3",
-    param1: 48,
-    param2: 52,
-    param3: 78,
-    timestamp: "date",
-    date: "August 10 2020"
-  },
-  {
-    id: "sensorid4",
-    param1: 67,
-    param2: 90,
-    param3: 54,
-    timestamp: "date",
-    date: "September 27 2020"
-  },
-  {
-    id: "sensorid4",
-    param1: 32,
-    param2: 67,
-    param3: 66,
-    timestamp: "date",
-    date: "September 27 2020"
-  }
-];
-const groupedData = sensorData.reduce((acc, curr) => {
-  const { id } = curr;
-  console.log("acc", acc);
-  if (!acc[id]) {
-    acc[id] = {
-      id,
-      data: [curr]
-    };
-  } else {
-    acc[id].data.push(curr);
-  }
-  return acc;
-}, {});
-const generateRandomColor = (index) =>
-  `rgb(${(index + 1) * 25}, ${(index + 1) * 50}, ${(index + 1) * 100})`;
-//@ts-ignore
-const datasets = Object.values(groupedData).map(({ id, data }, index) => ({
-  label: id,
-  data: data.map((d: { param1: any }) => d.param1),
-  borderColor: generateRandomColor(index),
-  fill: false,
-  lineTension: 0.1,
-  backgroundColor: `rgb(${(index + 1) * 25}, ${(index + 1) * 50}, ${
-    (index + 1) * 100
-  })`,
-  borderCapStyle: "butt",
-  borderDash: [],
-  borderDashOffset: 0.0,
-  borderJoinStyle: "miter" as const,
-  pointBorderColor: generateRandomColor(index),
-  pointBackgroundColor: "#fff" as const,
-  pointBorderWidth: 1,
-  pointHoverRadius: 5,
-  pointHoverBackgroundColor: generateRandomColor(index),
-  pointHoverBorderColor: generateRandomColor(index),
-  pointHoverBorderWidth: 2,
-  pointRadius: 1,
-  pointHitRadius: 10,
-  yAxisID: "y" as const
-}));
-console.log("datasets from chatgpt", datasets);
-const dataTest = {
-  labels: labels,
-  datasets
+  };
 };
 
-// const dataTest = {
-//   labels: labels,
-//   datasets: [
-//     {
-//       label: "Sensor1" as const,
-//       fill: false,
-//       lineTension: 0.1,
-//       backgroundColor: "rgba(75,192,192,0.4)" as const,
-//       borderColor: "rgba(75,192,192,1)" as const,
-//       borderCapStyle: "butt" as const,
-//       borderDash: [],
-//       borderDashOffset: 0.0,
-//       borderJoinStyle: "miter" as const,
-//       pointBorderColor: "rgba(75,192,192,1)" as const,
-//       pointBackgroundColor: "#fff" as const,
-//       pointBorderWidth: 1,
-//       pointHoverRadius: 5,
-//       pointHoverBackgroundColor: "rgba(75,192,192,1)" as const,
-//       pointHoverBorderColor: "rgba(220,220,220,1)" as const,
-//       pointHoverBorderWidth: 2,
-//       pointRadius: 1,
-//       pointHitRadius: 10,
-//       data: [65, 59, 80, 81, 56, 55, 40],
-//       yAxisID: "y" as const
-//     },
-//     {
-//       label: "Sensor2" as const,
-//       fill: false,
-//       lineTension: 0.1,
-//       backgroundColor: "rgba(53, 162, 235, 0.5)" as const,
-//       borderColor: "rgb(53, 162, 235)" as const,
-//       borderCapStyle: "butt" as const,
-//       borderDash: [],
-//       borderDashOffset: 0.0,
-//       borderJoinStyle: "miter" as const,
-//       pointBorderColor: "rgba(75,192,192,1)" as const,
-//       pointBackgroundColor: "#fff" as const,
-//       pointBorderWidth: 1,
-//       pointHoverRadius: 5,
-//       pointHoverBackgroundColor: "rgb(53, 162, 235)" as const,
-//       pointHoverBorderColor: "rgba(220,220,220,1)" as const,
-//       pointHoverBorderWidth: 2,
-//       pointRadius: 1,
-//       pointHitRadius: 10,
-//       data: [35, 47, 76, 81, 90, 23, 40],
-//       yAxisID: "y" as const
-//     }
-//   ]
-// };
-// TODO move function to utils
+// TODO move functions to utils
 async function multiFetcher(...urls: string[]) {
   const promises: string | Device[] = [];
   const deviceArrays = await Promise.all(urls.map((url) => fetcher(url)));
   return promises.concat(...deviceArrays);
 }
-
+// @ts-ignore
+function mapNames(id) {
+  const DeviceNames: { [key: string]: string | null | undefined } = {
+    "AQY BD-1071": "AQY1 Indio",
+    "AQY BD-1080": "Mecca",
+    "AQY BD-1072": "AQY2 Indio",
+    "AQY BD-1065": "Fillmore St. and 52nd Ave",
+    "AQY BD-1092": "Mission San Jose",
+    "AQY BD-1074": "Salton Sea State Park",
+    "AQY BD-1094": "Thermal Airport",
+    "AQY BD-1063": "Torres Martinez - Headquarters",
+    "AQY BD-1152": "Torres Martinez - Near Shore",
+    "MOD-PM-00404": "Palm Desert"
+  };
+  return DeviceNames[id];
+}
 const AirQualityPlots = ({ devices }: { devices: airQualityDevices[] }) => {
-  const chartRef = useRef<{ chartInstance: Chart }>(null);
-
-  //   console.log("devices in plots", devices);
-  // need an array of sensor id's
-  const sensorUrls = devices.map(({ sensorId, value }) => {
+  const classes = useStyles();
+  const [selectedParam, setSelectedParam] = useState("O3");
+  const sensorUrls = devices.map(({ sensorId }) => {
     const sensorInfoArray = sensorId.split(":");
     const sensorIdList = determineSourceOfData(sensorInfoArray[0]);
     return sensorIdList;
   });
-  //send sensor ID's to determine source then fetch using multi-fetch
-  //   console.log("list of sensor Ids: ", sensorUrls);
-  //   TODO fix quant device data, its only returning 1 entry.
-  // const { data = [], error } = useSWR(
-  //   ["../api/aq/devices/aqmd/AQY%BD-1092"],
-  //   multiFetcher
-  // );
-  // useEffect(() => {
-  //   if (chartRef.current && data) {
-  //     chartRef.current.chartInstance.update();
-  //   }
-  // }, [data]);
-  // console.log("data for all sensors", data);
-  // =====================using dummy data =========================
+  // TODO fix quant device data, its only returning 1 entry.
+  const { data: sensorData = [], error } = useSWR(sensorUrls, multiFetcher);
+  // console.log("data for all sensors", sensorData);
   // @ts-ignore
+  const groupedData = useMemo(() => {
+    return sensorData.reduce((sensors, curr) => {
+      // @ts-ignore
+      const id = curr.DeviceID || curr.sn;
+      if (!sensors[id]) {
+        // @ts-ignore
+        sensors[id] = {
+          id,
+          name: mapNames(id),
+          data: [{ ...curr }]
+        };
+      } else {
+        // @ts-ignore
+        sensors[id].data.push({ ...curr });
+      }
+      return sensors;
+    }, []);
+  }, [sensorData]);
 
+  const handleTitleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSelectedParam(event.target.value as string);
+  };
+  console.log("grouped data: ", groupedData);
+  const datasets = useMemo(() => {
+    //@ts-ignore
+    return Object.values(groupedData).map(({ data, name }, index) => ({
+      label: name,
+      // Todo parameter will change here depending on selection
+      data: paramAQICalc(data),
+      borderColor: colors[index],
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: `${colors[index]}3F`,
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter" as const,
+      pointBorderColor: colors[index],
+      pointBackgroundColor: "#fff" as const,
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "blue",
+      pointHoverBorderColor: "#fff" as const,
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      yAxisID: "y" as const
+    }));
+  }, [groupedData]);
+
+  // console.log("datasets", datasets);
+  const chartData = {
+    datasets
+  };
   console.count("renders: ");
-  return <Line plugins={plugins} options={options} data={dataTest} />;
+  return (
+    <>
+      <FormControl className={classes.formControl}>
+        <Select
+          value={selectedParam}
+          onChange={handleTitleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          {Object.keys(paramAQIStandardMap).map((param, index) => (
+            <MenuItem key={`${index}${param}`} value={param}>
+              {param}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Line
+        redraw={true}
+        plugins={plugins}
+        // @ts-ignore
+        options={options(selectedParam)}
+        // @ts-ignore
+        data={chartData}
+      />
+    </>
+  );
 };
 
 export default AirQualityPlots;
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    minWidth: 120
+  },
+  selectEmpty: {}
+}));
