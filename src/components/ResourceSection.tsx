23bf4d93063ecacab531a16f7a218d3cc6c515d8
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-04-07 15:12:27 -0700

    refactor

---
+++ src/components/ResourceSection.tsx
@@ -0,0 +1,81 @@
+import { Typography, Container, Link, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+type Props = {
+  title: string;
+  body: string;
+  link: string;
+  download: boolean;
+};
+const ResourceSection = ({ title, body, link, download }: Props) => {
+  const classes = useStyles();
+  // TODO add Translations and import content from Contentful.
+
+  return (
+    <Container className={classes.resourceSection}>
+      {download ? (
+        <Button
+          className={classes.downloadButton}
+          variant="text"
+          color="inherit"
+          href={link}
+          disableFocusRipple
+          disableRipple
+          download={download}
+        >
+          <Typography
+            variant="h4"
+            component="header"
+            className={classes.header}
+            display="inline"
+          >
+            {title}
+          </Typography>
+        </Button>
+      ) : (
+        <Link
+          variant="inherit"
+          underline="none"
+          target={"_blank"}
+          rel="noopener"
+          href={link}
+          color="inherit"
+        >
+          <Typography
+            variant="h4"
+            component="header"
+            className={classes.header}
+            display="inline"
+          >
+            {title}
+          </Typography>
+        </Link>
+      )}
+      <Typography variant="body2" style={{ marginTop: "1rem" }}>
+        {body}
+      </Typography>
+    </Container>
+  );
+};
+const useStyles = makeStyles((theme) => ({
+  header: {
+    boxShadow: `inset 0 -5px 0 ${theme.palette.primary.light}`,
+    transition: "color 0.2s ease",
+    "&:hover": {
+      color: theme.palette.secondary.light
+    }
+  },
+  resourceSection: {
+    marginBottom: "1rem"
+  },
+  downloadButton: {
+    ...theme.typography.h4,
+    textTransform: "none",
+    justifyContent: "flex-start",
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  }
+}));
+
+export default ResourceSection;
--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,26 +1,13 @@
-import {
-  Typography,
-  Container,
-  Button,
-  Box,
-  Divider,
-  Grid,
-  ButtonBase,
-  IconButton
-} from "@material-ui/core";
-import { Card, CardActions, CardContent, CardMedia } from "@material-ui/core";
+import { Container, Button, Box } from "@material-ui/core";
+import Image from "next/image";
+import { Card, CardMedia } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import AssignmentOutlinedIcon from "@material-ui/icons/AssignmentOutlined";
-import LaunchOutlinedIcon from "@material-ui/icons/LaunchOutlined";
-import Skeleton from "@material-ui/lab/Skeleton";
 import Hero from "components/Hero";
 import Section from "components/Section";
 import Layout from "components/Layout";
-import SectionHeader from "components/SectionHeader";
 import Meta from "components/Meta";
 import Translation from "components/Translation";
-import { fetcher } from "utils";
-import useSWR from "swr";
+import ResourceSection from "components/ResourceSection";
 type ArticleData = {
   data: {
     key: string;
@@ -40,33 +27,8 @@ const Resources = () => {
   const classes = useStyles();
   const groupID = 4854089;
   const zoteroUrl = `https://api.zotero.org/groups/${groupID}/items/top?limit=10&order=dateModified&v=3`;
-
-  const { data = [], error, isValidating } = useSWR(zoteroUrl, fetcher);
   // TODO add Translations and import content from Contentful
-  const placeHolderList = Array.from({ length: 3 }).map((_, index) => (
-    <Grid container spacing={1} key={index} alignItems="center">
-      <Grid item>
-        <ButtonBase style={{ width: 80, height: 80 }}>
-          <Skeleton
-            animation="wave"
-            variant="text"
-            height="100%"
-            width="100%"
-          />
-        </ButtonBase>
-      </Grid>
-      <Grid item xs={12} sm container>
-        <Grid item xs container direction="column" spacing={2}>
-          <Grid item xs>
-            <Skeleton animation="wave" variant="text" height={25} />
-            <Skeleton animation="wave" variant="text" height={20} />
-            <Skeleton animation="wave" variant="text" height={10} />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
-  ));
-  //  TODO Refactor : Maybe add a card component...
+  //  TODO Refactor : Make dry...
   return (
     <Layout>
       <Meta title="Resources | Salton Sea Environmental Timeseries" />
@@ -105,20 +67,30 @@ const Resources = () => {
           <Box className={classes.flyerContainerWrapper}>
             <Box className={classes.flyerContainer}>
               <Card className={classes.backCard}>
-                <CardMedia
-                  component={"img"}
-                  className={classes.cover}
-                  image="/salton-sea-flyer-back.jpg"
-                  title="Alianza CV"
-                />
+                <CardMedia className={classes.imageContainer} component="div">
+                  <Image
+                    src="/salton-sea-flyer-back.jpg"
+                    alt="Alianza CV"
+                    layout="responsive"
+                    width={300}
+                    height={400}
+                    objectFit="cover"
+                    priority
+                  />
+                </CardMedia>
               </Card>
               <Card className={classes.frontCard} elevation={5}>
-                <CardMedia
-                  component={"img"}
-                  className={classes.cover}
-                  image="/salton-sea-flyer-front.jpg"
-                  title="Alianza CV"
-                />
+                <CardMedia className={classes.imageContainer}>
+                  <Image
+                    src="/salton-sea-flyer-front.jpg"
+                    alt="Alianza CV"
+                    layout="responsive"
+                    width={300}
+                    height={400}
+                    objectFit="cover"
+                    priority
+                  />
+                </CardMedia>
               </Card>
             </Box>
           </Box>
@@ -127,95 +99,23 @@ const Resources = () => {
               Download Our Flyer
             </Button>
           </Box>
-          <SectionHeader
+          <ResourceSection
             title={"Scientific Articles"}
-            titleProps={{
-              align: "center",
-              className: classes.section,
-              display: "inline"
-            }}
-            paddingTop="1rem"
-            display="flex"
-            justifyContent="center"
-            size={4}
+            body="This links to a repository in Zotero of scientific articles. You
+              can sign up for an account and request membership in the
+              repository to get free access to PDFs of scientific articles. Feel
+              free to add articles to this collaborative repository or reach out
+              if there is an article that you want to read but don’t have access
+              to!"
+            link={`https://www.zotero.org/groups/${groupID}/saltonseascience/`}
+            download={false}
           />
-          <Box className={classes.zoteroContainer}>
-            <Card className={classes.zotero}>
-              <CardContent>
-                {isValidating
-                  ? placeHolderList
-                  : data.map(
-                      ({
-                        data: article,
-                        meta,
-                        links: { alternate }
-                      }: ArticleData) => (
-                        <Box key={article.key} style={{ marginBottom: "1rem" }}>
-                          <Grid container spacing={1} alignItems="center">
-                            <Grid item>
-                              <IconButton href={alternate.href} target="_blank">
-                                <AssignmentOutlinedIcon
-                                  fontSize="large"
-                                  color="secondary"
-                                />
-                              </IconButton>
-                            </Grid>
-                            <Grid item xs={12} sm container>
-                              <Grid
-                                item
-                                xs
-                                container
-                                direction="column"
-                                spacing={2}
-                              >
-                                <Grid item xs>
-                                  <Typography
-                                    variant="subtitle1"
-                                    component="div"
-                                  >
-                                    {article.title}
-                                  </Typography>
-                                  <Typography variant="body2" gutterBottom>
-                                    {meta.creatorSummary} • {article.date}
-                                  </Typography>
-                                  <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                  >
-                                    {article.publicationTitle &&
-                                      article.publicationTitle}
-                                  </Typography>
-                                </Grid>
-                              </Grid>
-                            </Grid>
-                          </Grid>
-                        </Box>
-                      )
-                    )}
-                {error && (
-                  <Typography>Error retrieving Scientific Articles</Typography>
-                )}
-                <Divider />
-                <Typography variant="caption">
-                  *List of articles above were gathered from Zotero; a tool used
-                  to collect, organize, annotate, cite, and share research.
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing className={classes.zoteroButton}>
-                <Button
-                  endIcon={<LaunchOutlinedIcon />}
-                  target="_blank"
-                  size="small"
-                  variant="contained"
-                  color="primary"
-                  href={`https://www.zotero.org/groups/${groupID}/saltonseascience/`}
-                  download
-                >
-                  Zotero
-                </Button>
-              </CardActions>
-            </Card>
-          </Box>
+          <ResourceSection
+            title={"2023 Salton Sea TMDL Public Letter"}
+            body="Download PDF Establishment of Salton Sea TMDLs in 2023"
+            link={"/salton-sea-mdl-public-letter-2023.pdf"}
+            download={true}
+          />
         </Container>
       </Section>
     </Layout>
@@ -223,7 +123,11 @@ const Resources = () => {
 };
 const useStyles = makeStyles((theme) => ({
   header: {
-    boxShadow: `inset 0 -5px 0 ${theme.palette.primary.light}`
+    boxShadow: `inset 0 -5px 0 ${theme.palette.primary.light}`,
+    transition: "color 0.2s ease",
+    "&:hover": {
+      color: theme.palette.secondary.light
+    }
   },
   section: {
     boxShadow: `inset 0 -5px 0 ${theme.palette.secondary.light}`
@@ -263,8 +167,15 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: "row"
     }
   },
+  imageContainer: {
+    position: "relative",
+    width: "100%",
+    height: "100%"
+  },
   frontCard: {
     width: 300,
+    minHeight: 400,
+    maxHeight: 480,
     margin: "1rem 0",
     transition: "transform 300ms",
     [theme.breakpoints.up("sm")]: {
@@ -279,6 +190,8 @@ const useStyles = makeStyles((theme) => ({
   },
   backCard: {
     width: 300,
+    minHeight: 400,
+    maxHeight: 480,
     margin: "1rem 0",
     transition: "transform 300ms",
     [theme.breakpoints.up("sm")]: {
@@ -294,3 +207,109 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default Resources;
+
+// TODO: Bottom component may be used in the future once Zotero list is confirmed
+// const { data = [], error, isValidating } = useSWR(zoteroUrl, fetcher);
+// TODO add Translations and import content from Contentful
+// const placeHolderList = Array.from({ length: 3 }).map((_, index) => (
+//   <Grid container spacing={1} key={index} alignItems="center">
+//     <Grid item>
+//       <ButtonBase style={{ width: 80, height: 80 }}>
+//         <Skeleton
+//           animation="wave"
+//           variant="text"
+//           height="100%"
+//           width="100%"
+//         />
+//       </ButtonBase>
+//     </Grid>
+//     <Grid item xs={12} sm container>
+//       <Grid item xs container direction="column" spacing={2}>
+//         <Grid item xs>
+//           <Skeleton animation="wave" variant="text" height={25} />
+//           <Skeleton animation="wave" variant="text" height={20} />
+//           <Skeleton animation="wave" variant="text" height={10} />
+//         </Grid>
+//       </Grid>
+//     </Grid>
+//   </Grid>
+// ));
+{
+  /* <Box className={classes.zoteroContainer}>
+<Card className={classes.zotero}>
+  <CardContent>
+    {isValidating
+      ? placeHolderList
+      : data.map(
+          ({
+            data: article,
+            meta,
+            links: { alternate }
+          }: ArticleData) => (
+            <Box key={article.key} style={{ marginBottom: "1rem" }}>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item>
+                  <IconButton href={alternate.href} target="_blank">
+                    <AssignmentOutlinedIcon
+                      fontSize="large"
+                      color="secondary"
+                    />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={12} sm container>
+                  <Grid
+                    item
+                    xs
+                    container
+                    direction="column"
+                    spacing={2}
+                  >
+                    <Grid item xs>
+                      <Typography
+                        variant="subtitle1"
+                        component="div"
+                      >
+                        {article.title}
+                      </Typography>
+                      <Typography variant="body2" gutterBottom>
+                        {meta.creatorSummary} • {article.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                      >
+                        {article.publicationTitle &&
+                          article.publicationTitle}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Box>
+          )
+        )}
+    {error && (
+      <Typography>Error retrieving Scientific Articles</Typography>
+    )}
+    <Divider />
+    <Typography variant="caption">
+      *List of articles above were gathered from Zotero; a tool used
+      to collect, organize, annotate, cite, and share research.
+    </Typography>
+  </CardContent>
+  <CardActions disableSpacing className={classes.zoteroButton}>
+    <Button
+      endIcon={<LaunchOutlinedIcon />}
+      target="_blank"
+      size="small"
+      variant="contained"
+      color="primary"
+      href={`https://www.zotero.org/groups/${groupID}/saltonseascience/`}
+      download
+    >
+      Zotero
+    </Button>
+  </CardActions>
+</Card>
+</Box>  */
+}
