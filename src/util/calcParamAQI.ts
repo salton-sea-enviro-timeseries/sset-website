2935d2e64c458d5107909a0a58127f7190e4e5db
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-04-11 16:50:15 -0700

    Fix timestamp to local

--- src/util/calcParamAQI.ts
+++ src/util/calcParamAQI.ts
@@ -1,7 +1,8 @@
 import { CommonDeviceType } from "types";
+
 export function calcParamAQI(data: CommonDeviceType[]) {
   return data.map(
-    ({ EndDate, timestamp, O3, NO2, PM10, "PM2.5": PM2_5, PM1 }) => {
+    ({ EndDate, timestamp_local, O3, NO2, PM10, "PM2.5": PM2_5, PM1 }) => {
       return {
         O3: O3 ? Math.round((O3 / 70) * 100) : undefined,
         // leaving NO2 as is
@@ -9,7 +10,7 @@ export function calcParamAQI(data: CommonDeviceType[]) {
         PM10: PM10 ? Math.round((PM10 / 150) * 100) : undefined,
         PM2_5: PM2_5 ? Math.round((PM2_5 / 35) * 100) : undefined,
         PM1,
-        x: timestamp ? timestamp : EndDate
+        x: timestamp_local ? timestamp_local : EndDate
       };
     }
   );
