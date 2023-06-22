464cc85b090634edbed7d798262216540807a136
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-06-22 16:44:17 -0700

    fix: refactor

--- src/util/calcParamAQI.ts
+++ src/util/calcParamAQI.ts
@@ -4,11 +4,11 @@ export function calcParamAQI(data: CommonDeviceType[]) {
   return data.map(
     ({ EndDate, timestamp_local, O3, NO2, PM10, "PM2.5": PM2_5, PM1 }) => {
       return {
-        O3: O3 ? Math.round((O3 / 70) * 100) : undefined,
+        O3: O3 && Math.round((O3 / 70) * 100),
         // leaving NO2 as is
-        NO2: NO2 ? Math.round(NO2) : undefined,
-        PM10: PM10 ? Math.round((PM10 / 150) * 100) : undefined,
-        PM2_5: PM2_5 ? Math.round((PM2_5 / 35) * 100) : undefined,
+        NO2: NO2 && Math.round(NO2),
+        PM10: PM10 && Math.round((PM10 / 150) * 100),
+        PM2_5: PM2_5 && Math.round((PM2_5 / 35) * 100),
         PM1,
         x: timestamp_local ? timestamp_local : EndDate
       };
