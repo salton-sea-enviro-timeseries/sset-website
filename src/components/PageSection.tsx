9a23490210bf2066f7164b1430a8cd2b10c005af
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-08-28 12:53:50 -0700

    Add timeline image to first section  - refactor section content and grad image content

--- src/components/PageSection.tsx
+++ src/components/PageSection.tsx
@@ -1,23 +1,97 @@
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
-import { colors } from "@material-ui/core";
+import { Divider, colors } from "@material-ui/core";
 import FeaturedNewsFeed from "components/InTheNews/FeaturedNewsFeed";
 import Section from "components/Section";
 import SectionHeader from "components/SectionHeader";
 import { Typography } from "@material-ui/core";
 import { MediaObject } from "types";
 import Image from "next/image";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 // TODO: add links to definitions
 // or add a glossary section ???
 type Props = {
   newsMediaData?: MediaObject[];
-  bodyText: any;
+  bodyText: React.ReactNode | null;
   title: string;
   section: number;
   gradImages?: string[];
+  id: string;
+};
+interface GradSectionClasses {
+  gradContainer: string;
+  imageWrapper: string;
+}
+const generateContent = (
+  classes: { timeLineImage: string; graphDivider: string },
+  id: string,
+  newsSection?: MediaObject[],
+  bodyText?: React.ReactNode | null
+) => {
+  return newsSection ? (
+    <>
+      <FeaturedNewsFeed newsMediaData={newsSection} />
+      <Box mt="1em">
+        <Typography component="p" align="center">
+          *Many of these articles use highly negative language such as “toxic”
+          and “death pit” in referring to the Salton Sea, portraying a hopeless
+          situation. While we are very aware of the environmental state of the
+          Salton Sea, we believe such language is damaging to the surrounding
+          community and discourages remediation efforts. We encourage the use of
+          positive language that centers not just biodiversity but the community
+          and their stories who deserve a restored sea.
+        </Typography>
+      </Box>
+    </>
+  ) : id === "section-0" ? (
+    <>
+      <Typography component="div">{bodyText}</Typography>
+      <Box display="flex" justifyContent={"space-evenly"}>
+        <Divider color={"red"} className={classes.graphDivider} />
+        <FiberManualRecordIcon color="primary" fontSize="small" />
+        <Divider className={classes.graphDivider} />
+      </Box>
+      <Box
+        display={"flex"}
+        alignContent={"center"}
+        className={classes.timeLineImage}
+      >
+        <Image
+          src="/salton-sea-timeline.png"
+          alt="salton sea timeline"
+          layout="fill"
+          objectFit="contain"
+        />
+      </Box>
+    </>
+  ) : (
+    <Typography component="div">{bodyText}</Typography>
+  );
+};
+const generateGradSection = (
+  classes: GradSectionClasses,
+  gradImages?: string[]
+) => {
+  return (
+    gradImages && (
+      <Box className={classes.gradContainer}>
+        {gradImages.map((item, index) => (
+          <div key={index} className={classes.imageWrapper}>
+            <Image
+              src={`https:${item}`}
+              alt={"grad image"}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        ))}
+      </Box>
+    )
+  );
 };
 const PageSection: React.FC<Props> = ({
+  id,
   bodyText,
   title,
   newsMediaData,
@@ -44,39 +118,8 @@ const PageSection: React.FC<Props> = ({
             justifyContent="center"
             size={4}
           />
-          {newsMediaData ? (
-            <>
-              <FeaturedNewsFeed newsMediaData={newsMediaData} />
-              <Box mt="1em">
-                <Typography component="p" align="center">
-                  *Many of these articles use highly negative language such as
-                  “toxic” and “death pit” in referring to the Salton Sea,
-                  portraying a hopeless situation. While we are very aware of
-                  the environmental state of the Salton Sea, we believe such
-                  language is damaging to the surrounding community and
-                  discourages remediation efforts. We encourage the use of
-                  positive language that centers not just biodiversity but the
-                  community and their stories who deserve a restored sea.
-                </Typography>
-              </Box>
-            </>
-          ) : (
-            <Typography component="div">{bodyText}</Typography>
-          )}
-          {gradImages && (
-            <Box className={classes.gradContainer}>
-              {gradImages.map((item, index) => (
-                <div key={index} className={classes.imageWrapper}>
-                  <Image
-                    src={`https:${item}`}
-                    alt={"grad image"}
-                    layout="fill"
-                    objectFit="contain"
-                  />
-                </div>
-              ))}
-            </Box>
-          )}
+          {generateContent(classes, id, newsMediaData, bodyText)}
+          {generateGradSection(classes, gradImages)}
         </Box>
       </Container>
     </Section>
@@ -92,14 +135,31 @@ const useStyles = makeStyles((theme) => ({
   gradContainer: {
     display: "flex",
     justifyContent: "space-evenly",
-
     flexWrap: "wrap"
   },
   imageWrapper: {
-    height: 600,
-    width: 400,
+    minHeight: 400,
+    width: 300,
     position: "relative",
-    marginTop: "1em"
+    marginTop: "1rem"
+  },
+  timeLineImage: {
+    [theme.breakpoints.down("sm")]: {
+      height: 300
+    },
+    [theme.breakpoints.up("sm")]: {
+      height: 600
+    },
+    width: "100%",
+    marginTop: "2rem",
+    position: "relative"
+  },
+  graphDivider: {
+    margin: "auto",
+    width: "50%",
+    height: 2,
+    borderRadius: 50,
+    background: "#BCBCBC"
   }
 }));
 export default PageSection;
