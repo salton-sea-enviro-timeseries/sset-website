39b107ec51419ab882e9838cb5b36ebb38928515
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-11-26 18:04:45 -0800

    fix function to correctly determine api route based on sensor id prefix

--- src/lib/determineSourceOfData.ts
+++ src/lib/determineSourceOfData.ts
@@ -1,8 +1,18 @@
 function determineSourceOfData(sensor: string) {
-  const regex = /^M/g;
-  return sensor.match(regex)
-    ? `../api/aq/devices/quant/${sensor}`
-    : `../api/aq/devices/aqmd/${sensor}`;
+  // Regular expressions for each sensor type
+  const quantRegex = /^MOD/g;
+  const aqmdRegex = /^AQY/g;
+  const aeroqualRegex = /^AQS1/g;
+  // Determine the API route based on the sensor ID prefix
+  if (quantRegex.test(sensor)) {
+    return `../api/aq/devices/quant/${sensor}`;
+  } else if (aqmdRegex.test(sensor)) {
+    return `../api/aq/devices/aqmd/${sensor}`;
+  } else if (aeroqualRegex.test(sensor)) {
+    return `../api/aq/devices/aeroqual/${sensor}`;
+  } else {
+    throw new Error("Unknown sensor ID prefix");
+  }
 }
 
 export default determineSourceOfData;
