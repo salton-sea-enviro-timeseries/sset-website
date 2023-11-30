6134a6d59150e818abb2ba092237a2c24e36c204
Gerardo Solis <gerardoa.solis@yahoo.com>
2023-11-30 12:29:30 -0800

    test netlify with getCookies removed

--- src/pages/api/aq/devices/aeroqual/[sensorId].ts
+++ src/pages/api/aq/devices/aeroqual/[sensorId].ts
@@ -5,6 +5,7 @@ import {
 } from "lib/aeroqual";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getCookies } from "util/getCookies";
+import { loginAndGetCookies } from "util/loginAndGetCookies";
 function transformData(originalData: OriginalData): TransformedData[] {
   if (!originalData.Instruments || originalData.Instruments.length === 0) {
     return [];
@@ -27,12 +28,22 @@ export default async function handler(
   if (method !== "GET")
     return res.status(405).end(`Method ${method} Not Allowed`);
   try {
-    const cookies = await getCookies(req, "aq_auth");
-    let cookieString = `${cookies}; Path=/; Secure; HttpOnly; SameSite=None`;
-    if (COOKIE_DOMAIN) {
-      cookieString += `;Domain=${COOKIE_DOMAIN}`;
+    let cookies = req.headers.cookie || "";
+    if (!cookies) {
+      cookies = await loginAndGetCookies();
     }
-    res.setHeader("set-cookie", cookieString);
+    // const cookies = await getCookies(req, "aq_auth");
+    const cookieRegex = new RegExp(`aq_auth=([^;]+)`, "i");
+    const cookieMatch = cookies.match(cookieRegex);
+    if (cookieMatch && cookieMatch[1] && COOKIE_DOMAIN) {
+      const cookieString = `aq_auth=${cookieMatch[1]}; Path=/; Secure; HttpOnly; SameSite=None`;
+      res.setHeader("set-cookie", cookieString);
+    }
+    // let cookieString = `${cookies}; Path=/; Secure; HttpOnly; SameSite=None`;
+    // if (COOKIE_DOMAIN) {
+    //   cookieString += `;Domain=${COOKIE_DOMAIN}`;
+    // }
+    // res.setHeader("set-cookie", cookieString);
     const data = await getAeroqualDeviceData({
       sensorId: query.sensorId as string,
       startDate: query.startDate as string,
@@ -51,3 +62,36 @@ export default async function handler(
     }
   }
 }
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { method, query } = req;
+//   if (method !== "GET")
+//     return res.status(405).end(`Method ${method} Not Allowed`);
+//   try {
+//     const cookies = await getCookies(req, "aq_auth");
+//     let cookieString = `${cookies}; Path=/; Secure; HttpOnly; SameSite=None`;
+//     if (COOKIE_DOMAIN) {
+//       cookieString += `;Domain=${COOKIE_DOMAIN}`;
+//     }
+//     res.setHeader("set-cookie", cookieString);
+//     const data = await getAeroqualDeviceData({
+//       sensorId: query.sensorId as string,
+//       startDate: query.startDate as string,
+//       endDate: query.endDate as string,
+//       cookies
+//     });
+//     const reformatData = transformData(data);
+//     return res.status(200).json(reformatData);
+//   } catch (err) {
+//     if (err instanceof Error) {
+//       console.error(err);
+//       return res.status(500).json({ message: err.message });
+//     } else {
+//       console.error("An unexpected error occurred:", err);
+//       return res.status(500).json({ message: "An unexpected error occurred" });
+//     }
+//   }
+// }
