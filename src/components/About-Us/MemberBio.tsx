52a78bfb9fb68719d0098b7a23805149bb7538fe
Gerardo Solis <gerardoa.solis@yahoo.com>
2024-12-22 19:23:31 -0800

    update memberBio component with styled components

--- src/components/About-Us/MemberBio.tsx
+++ src/components/About-Us/MemberBio.tsx
@@ -1,11 +1,6 @@
-import {
-  Box,
-  Avatar,
-  Typography,
-  makeStyles,
-  Divider
-} from "@material-ui/core";
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import { Box, Avatar, Typography, makeStyles, Divider } from "@mui/material";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import { styled } from "@mui/material/styles";
 type BioProps = {
   firstName: string;
   lastName: string;
@@ -20,95 +15,80 @@ const MemberBio: React.FC<BioProps> = ({
   bio,
   img
 }) => {
-  const classes = useStyles();
   return (
-    <Box className={classes.bioWrapper}>
-      <Box className={classes.teamMemberBioSection}>
-        <Typography variant="h5" display="block" className={classes.nameStyle}>
+    <BioWrapper>
+      <TeamMemberBioSection>
+        <NameStyle variant="h5" display="block">
           {firstName} {lastName}
-        </Typography>
-        <Box className={classes.dividerWrapper}>
-          <Divider className={classes.divider} />
+        </NameStyle>
+        <DividerWrapper>
+          <StyledDivider />
           <FiberManualRecordIcon color="primary" fontSize="small" />
-          <Divider className={classes.divider} />
-        </Box>
-      </Box>
+          <StyledDivider />
+        </DividerWrapper>
+      </TeamMemberBioSection>
 
-      <Box className={classes.bioContainer}>
-        <Avatar
-          alt={`${firstName} ${lastName}`}
-          src={img}
-          className={classes.avatar}
-        >
+      <BioContainer>
+        <StyledAvatar alt={`${firstName} ${lastName}`} src={img}>
           <Typography variant="h4">
             {`${firstName[0]}${lastName[0]}`}
           </Typography>
-        </Avatar>
+        </StyledAvatar>
         <Box>
-          <Typography
-            variant="subtitle1"
-            display="block"
-            className={classes.titleStyles}
-          >
+          <TitleStyles variant="subtitle1" display="block">
             {title}
-          </Typography>
+          </TitleStyles>
           <Typography variant="body1" display="inline">
             {bio}
           </Typography>
         </Box>
-      </Box>
-    </Box>
+      </BioContainer>
+    </BioWrapper>
   );
 };
-
-const useStyles = makeStyles((theme) => ({
-  nameStyle: {
-    marginRight: theme.spacing(2),
-    minWidth: "200px"
-  },
-  titleStyles: { fontWeight: "bold" },
-  dividerContainer: {
-    display: "flex",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "center"
-  },
-  teamMemberBioSection: {
-    display: "flex",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "space-between",
-    marginBottom: theme.spacing(2)
-  },
-  dividerWrapper: {
-    display: "flex",
-    alignItems: "center",
-    flexGrow: 1,
-    justifyContent: "center"
-  },
-  divider: {
-    flexGrow: 1,
-    height: 1,
-    borderRadius: 50,
-    background: "#BCBCBC"
-  },
-  avatar: {
-    width: theme.spacing(25),
-    height: theme.spacing(25),
-    marginRight: theme.spacing(2)
-  },
-  avatarOdd: {
-    width: theme.spacing(25),
-    height: theme.spacing(25),
-    float: "right",
-    shapeOutside: "circle(50%)",
-    marginRight: "1rem"
-  },
-  bioContainer: {
-    display: "flex"
-  },
-  bioWrapper: {
-    marginTop: theme.spacing(4)
-  }
+const BioWrapper = styled(Box)(({ theme }) => ({
+  marginTop: theme.spacing(4)
 }));
+
+const TeamMemberBioSection = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  justifyContent: "space-between",
+  marginBottom: theme.spacing(2)
+}));
+
+const NameStyle = styled(Typography)(({ theme }) => ({
+  marginRight: theme.spacing(2),
+  minWidth: "200px"
+}));
+
+const DividerWrapper = styled(Box)(() => ({
+  display: "flex",
+  alignItems: "center",
+  flexGrow: 1,
+  justifyContent: "center"
+}));
+
+const StyledDivider = styled(Divider)(() => ({
+  flexGrow: 1,
+  height: 1,
+  borderRadius: 50,
+  background: "#BCBCBC"
+}));
+
+const BioContainer = styled(Box)(() => ({
+  display: "flex"
+}));
+
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  width: theme.spacing(25),
+  height: theme.spacing(25),
+  marginRight: theme.spacing(2)
+}));
+
+const TitleStyles = styled(Typography)(() => ({
+  fontWeight: "bold"
+}));
+
 export default MemberBio;
