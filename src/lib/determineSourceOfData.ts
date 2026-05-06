674a3d37f28a90f3131a66ceac32e2b3c763ddd2
Gerardo Solis <gerardoa.solis@yahoo.com>
2026-05-06 15:32:01 -0700

    fix aqmd calls

--- src/lib/determineSourceOfData.ts
+++ src/lib/determineSourceOfData.ts
@@ -3,17 +3,19 @@ function determineSourceOfData(sensor: string) {
     console.warn("Invalid or missing sensor ID.");
     return null;
   }
-  // Regular expressions for each sensor type
+
   const quantRegex = /^MOD/g;
   const aqmdRegex = /^AQY/g;
   const aeroqualRegex = /^AQS1/g;
-  // Determine the API route based on the sensor ID prefix
+
+  const encodedSensor = encodeURIComponent(sensor);
+
   if (quantRegex.test(sensor)) {
-    return `../api/aq/devices/quant/${sensor}`;
+    return `../api/aq/devices/quant/${encodedSensor}`;
   } else if (aqmdRegex.test(sensor)) {
-    return `../api/aq/devices/aqmd/${sensor}`;
+    return `../api/aq/devices/aqmd/${encodedSensor}`;
   } else if (aeroqualRegex.test(sensor)) {
-    return `../api/aq/devices/aeroqual/${sensor}`;
+    return `../api/aq/devices/aeroqual/${encodedSensor}`;
   } else {
     throw new Error("Unknown sensor ID prefix");
   }
